947c03883bd5f5158308b537b3df402d0d0e8754
Hibi Miyazaki <hibi.miyazaki@gmail.com>
2019-05-04 16:41:33 +0900

    add .env

--- services/firebase.js
+++ services/firebase.js
@@ -1,5 +1,20 @@
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDERID,
+} from 'react-native-dotenv'
 import firebase from 'firebase';
 
-var config = {};
+var config = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDERID
+};
 
 export default firebase.initializeApp(config);
